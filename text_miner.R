29fa16fcd8f7d4b6c7f0330017cb4ba490ec5979
Greg Kinman - B48856 <B48856@freescale.com>
2014-07-21 11:21:01 -0500

    Initial text miner created.

---
+++ text_miner.R
@@ -0,0 +1,59 @@
+"
+Greg Kinman - B48856
+Freescale Semiconductor, Inc.
+
+text_miner.R
+
+Mines web-scraped text.
+"
+
+setwd("c:/users/b48856/documents/sentiment/sentiment_mining/")
+
+library(tm)
+
+main <- function() {
+    
+    "
+    Driver function for text processing.
+    "
+    
+    test_data <- processor("test_text.csv")
+    
+}
+
+processor <- function(csv_filename) {
+    
+    "
+    Stems and removes stopwords from the text of the first column of a .csv file.
+    
+    Input:
+    1. csv_filename:    a string: the name of the tab-delimited .csv file that contains the data
+    
+    Output:
+    1. final_data:      a data frame: a data frame containing the processed text data and its metadata
+    "
+    
+    # Loads the file data into a data frame.
+    original_data <- read.csv(csv_filename)
+    
+    # Pulls just the text column (the leftmost column) and stems and removes stopwords.
+    just_text <- original_data[,1]
+    text_corpus <- Corpus(DataframeSource(data.frame(just_text)))
+    stemmed_text <- tm_map(text_corpus, stemDocument, language="english")
+    
+    removed_text <- tm_map(stemmed_text, removeWords, stopwords("english"))
+    corpus_dict <- as.character(text_corpus)
+    print(corpus_dict)
+    new_text <- tm_map(removed_text, stemCompletion, dictionary=corpus_dict)
+    #new_text <- tm_map(stemmed_text, removeWords, stopwords("english"))
+    
+    # Replaces the leftmost column in the original data frame with the processed text.
+    metadata <- original_data[,-1]
+    new_text_frame <- t(as.data.frame(new_text))
+    final_data <- data.frame(new_text_frame, metadata)
+    
+    return(final_data)
+    
+}
+
+main()
